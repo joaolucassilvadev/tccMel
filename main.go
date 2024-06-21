0dac00fd7f8c5361e2e4f583493fcedea93fce5c
joaolucassilvadev <jls.silva@discente.ufma.br>
2024-06-21 08:36:35 -0300

    new port

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"microservicos.com/cmd/config"
 	"microservicos.com/cmd/routes"
@@ -15,5 +18,18 @@ func main() {
 	routes.Healthy(r)
 	routes.CategoryRoutes(r)
 
-	r.Run(":10000")
+	host := os.Getenv("HOST")
+	if host == "" {
+		host = "0.0.0.0"
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8089"
+	}
+
+	address := host + ":" + port
+	if err := r.Run(address); err != nil {
+		log.Panicf("error: %s", err)
+	}
 }
